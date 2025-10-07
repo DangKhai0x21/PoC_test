1397a5cd62dd65d0ff445b0a45e36d483d1930a6
DangKhai <32236617+DangKhai0x21@users.noreply.github.com>
2025-10-07 11:55:57 +0700

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,1 +1,7 @@
-<html>ahih</html>
+// alert-leen.js
+// Simple JavaScript file that shows an alert with the message "leen ahihi".
+// Usage: include this file in an HTML page with <script src="alert-leen.js"></script>
+(function() {
+  'use strict';
+  alert('leen ahihi');
+})();
